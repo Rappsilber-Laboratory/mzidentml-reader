6a8550a9311ad16d1c5c6365a36663422c19899b
Colin Combe <colin.combe@ed.ac.uk>
2026-07-15 11:41:35 +0200

    add psims to setup.py, increment version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_version(rel_path):
 
 setup(
     name="mzidentml-reader",
-    version="0.4.21",
+    version="0.4.23",
     description="mzidentml-reader uses pyteomics (https://pyteomics.readthedocs.io/en/latest/index.html) to "
     "parse mzIdentML files (v1.2.0 and v1.3.0) and extract crosslink information. Results are written to a "
     "relational database (PostgreSQL or SQLite) using sqlalchemy.",
@@ -41,6 +41,7 @@ setup(
         "pandas>=0.21.0",
         "pymzml>=0.7.8",
         "pyteomics>=5.0.0",
+        "psims",
         "requests>=2.31.0",
         "urllib3>=2.6.3",
         "psycopg2-binary",
